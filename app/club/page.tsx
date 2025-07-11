a92c34bd8cdd9f7abccdfc6e76ae137ead62bfcc
[Faygo1337] <[maximgustynov@gmail.com]>
2025-07-11 04:10:31 +0300

    feat: updated the video link

--- app/club/page.tsx
+++ app/club/page.tsx
@@ -57,7 +57,7 @@ export default function ClubHomePage() {
               poster={poster}
             >
               <source
-                src="https://zyboer4evcqe2k0o.public.blob.vercel-storage.com/video/clubBg.mp4"
+                src="https://12ormbedoelpdllo.public.blob.vercel-storage.com/Club%20Background%20Video.mp4"
                 type="video/mp4"
               />
             </video>
